a614ee5f0ec442fc8a036c510f6f1e54c77993ff
Xuan Do <doxuancntp@gmail.com>
2019-05-30 16:49:42 -0400

    Mini project.

--- questioner-mini-project/js/app.js
+++ questioner-mini-project/js/app.js
@@ -1,6 +1,33 @@
 new Vue({
     el: "#app",
     data: {
-        
+        question: '',
+        answer: '',
+        image: ''
+    },
+    watch: {
+        question: function(newQuestion) {
+            this.answer = 'Waiting for you to start typing...';
+            this.getAnswer();
+        },
+    },
+    methods: {
+        getAnswer: _.debounce(function() {
+            let vm = this;
+
+            if (this.question.indexOf('?') === -1) {
+                vm.answer = 'Question usually have a questionmark';
+                return;
+            }
+            vm.answer = 'Thinking...';
+            axios.get('https://yesno.wtf/api').then(function(response) {
+                vm.answer = _.capitalize(response.data.answer);
+                vm.image = response.data.image;
+            }).catch(function(err) {
+                vm.answer = 'Error: ' + err;
+            });
+        }, 500),
+
+
     }
 });
