9d145ed2ef04641778f29f316007185b48fa78c5
Xuan Do <doxuancntp@gmail.com>
2019-05-30 12:05:58 -0400

    Handling Events.

--- input-events-transitions/js/app.js
+++ input-events-transitions/js/app.js
@@ -11,6 +11,25 @@ new Vue({
             {text: 'One', value: 'A'},
             {text: 'Two', value: 'B'},
             {text: 'Three', value: 'C'}
-        ]
+        ],
+        counter: 0
+    },
+    methods: {
+        greet: function(greeting, event) {
+            // alert(greeting);
+
+            console.log(event.target.className);
+        },
+        keypress: function(event) {
+            console.log("Pressed");
+
+            console.log(event.target.value);
+        },
+        keySubmit: function(event) {
+            console.log("Enter Hit....");
+        },
+        formSubmit: function() {
+            alert(this.msg);
+        }
     }
 });
